a33c5a4a228502a7f1874ba3f51a970fde02f93b
Kevin Moore <moore.kevin.p@gmail.com>
2019-11-05 15:18:59 -0500

    added api methods

---
+++ src/api.js
@@ -0,0 +1,47 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/kevin/';
+
+//NEEDS ARGS
+const apiFetch = function() {
+  let error;
+  return fetch()
+    .then(res => {
+      if(!res.ok) {
+        error = {code: res.status};
+        if(!res.headers.get('content-type').includes('json')) {
+          return Promise.reject(error);
+        }
+      }
+      return res.json();
+    })
+    .then(data => {
+      if (error) {
+        error.message = data.message;
+        return Promise.reject(error);
+      }
+      return data;
+    });
+};
+
+const getBookmarks = function() {
+    return apiFetch(`${BASE_URL}/bookmarks`)
+};
+
+const createBookmark = function(title) {
+    const newBookmark = JSON.stringify({title});
+    return apiFetch(`${BASE_URL}/bookmarks`, {
+        method: 'POST',
+        headers: {'Content-Type: 'application/json},
+        body: newBookmark
+    });
+};
+const deleteBookmark = function(id) {
+    return apiFetch(BASE_URL + '/bookmarks/' +id, {
+        method: 'DELETE'
+    });
+};
+
+export {
+    getBookmarks,
+    createBookmark,
+    deleteBookmark
+};
---
+++ src/index.js
@@ -0,0 +1,39 @@
+//import statements
+
+//
+function renderBookmarks() {
+
+}
+
+//
+function filterBookmarks() {
+
+}
+
+//
+function addBookmark() {
+
+}
+
+//
+function expandBookmark() {
+
+}
+
+//
+function deleteBookmark() {
+
+}
+
+//
+const main = function() {
+    renderBookmarks();
+    filterBookmarks();
+    addBookmark();
+    expandBookmark();
+    deleteBookmark();
+}
+
+//call the main function
+
+//export statements
